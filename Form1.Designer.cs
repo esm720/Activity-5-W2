9d20f76b6c4f17353eee269ec7d6825eb6038766
esm720 <esm720@gmail.com>
2022-07-24 18:47:08 -0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Activity_5_W2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ProcessBut = new System.Windows.Forms.Button();
+            this.ExitBut = new System.Windows.Forms.Button();
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // ProcessBut
+            // 
+            this.ProcessBut.Location = new System.Drawing.Point(73, 129);
+            this.ProcessBut.Name = "ProcessBut";
+            this.ProcessBut.Size = new System.Drawing.Size(236, 98);
+            this.ProcessBut.TabIndex = 0;
+            this.ProcessBut.Text = "Process File...";
+            this.ProcessBut.UseVisualStyleBackColor = true;
+            this.ProcessBut.Click += new System.EventHandler(this.ProcessBut_Click);
+            // 
+            // ExitBut
+            // 
+            this.ExitBut.Location = new System.Drawing.Point(393, 129);
+            this.ExitBut.Name = "ExitBut";
+            this.ExitBut.Size = new System.Drawing.Size(241, 98);
+            this.ExitBut.TabIndex = 1;
+            this.ExitBut.Text = "Exit";
+            this.ExitBut.UseVisualStyleBackColor = true;
+            this.ExitBut.Click += new System.EventHandler(this.ExitBut_Click);
+            // 
+            // openFile
+            // 
+            this.openFile.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.ProcessBut;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.ExitBut;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ExitBut);
+            this.Controls.Add(this.ProcessBut);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ProcessBut;
+        private System.Windows.Forms.Button ExitBut;
+        private System.Windows.Forms.OpenFileDialog openFile;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Activity_5_W2
+{
+    
+
+    public partial class Form1 : Form
+    {
+        StreamReader inputFile;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ProcessBut_Click(object sender, EventArgs e)
+        {
+            String initialPhrase;
+            openFile.InitialDirectory = "C:\\Users\\esm72\\OneDrive\\Desktop\\CST-150\\Week 3";
+            openFile.ShowDialog();
+            inputFile = File.OpenText(openFile.FileName);
+            initialPhrase = inputFile.ReadToEnd();
+            MessageBox.Show("The file's content is: \n" + initialPhrase);
+
+            //Here we are calling on the methods to handle these tasks.
+            lowerCase(initialPhrase);
+            longestWord();
+            sortAlpha();
+            mostVowels();
+
+
+        }
+
+        //Method for making content all lowercase:
+        private void lowerCase(String initialPhrase)
+        {
+            String lowerCasePhrase = initialPhrase.ToLower();
+            MessageBox.Show("Content all Lowercase:\n" + lowerCasePhrase);
+        
+
+
+        }
+
+        //Method for finding longest word
+        private void longestWord()
+        {
+            inputFile = File.OpenText(openFile.FileName);
+
+            String longestWord = "";
+            String prevWord = " ";
+            String currentWord;
+            while (!inputFile.EndOfStream)
+            {
+                currentWord = inputFile.ReadLine();
+                if(currentWord.Length > prevWord.Length) //We used prevWord on multiple occasions to remember the last word we are comparing to.
+                {
+                    longestWord = currentWord;
+                    prevWord = currentWord;
+                }
+            }
+            MessageBox.Show("The longest word is:\n" + longestWord);
+        }
+
+
+        private void sortAlpha()
+        {
+            inputFile = File.OpenText(openFile.FileName);
+
+
+            String firstWord = "Z";
+            String lastWord = " ";
+            String currentWord;
+            String prevWord = "a"; 
+            
+            
+
+
+            while (!inputFile.EndOfStream)
+            {
+                currentWord = inputFile.ReadLine();
+
+
+                if (String.Compare(currentWord, prevWord) < 0) 
+                {
+                    if(String.Compare(currentWord, firstWord) < 0)
+                    {
+                        firstWord= currentWord;
+                    }
+                }
+                if (String.Compare(currentWord, prevWord) > 0)
+                {
+                  
+                    if (String.Compare(currentWord, lastWord) > 0)
+                    {
+                        lastWord = currentWord;
+                    }
+                }
+                prevWord = currentWord;
+
+
+            }
+
+            MessageBox.Show("The first and last word sorted alphabetically is:\n"+
+                "First: " + firstWord + "\nLast: " + lastWord);
+        }
+
+        private void mostVowels()
+        {
+            inputFile = File.OpenText(openFile.FileName);
+            String currentWord;
+            String mostVowWord = " ";
+            int vowelTotal = 0;
+            int prevWordTotal = 0;
+
+
+            while (!inputFile.EndOfStream)
+                
+            {
+                vowelTotal = 0;
+                currentWord = inputFile.ReadLine();
+                //Below we unrolled the for loop that would have ran the length of each word for debugging purposes only. it would have also used the OR operator to look for all vowels.
+                //comparing lowercase
+                if (currentWord.Contains("a"))
+                {
+                    vowelTotal++;
+                }
+                if (currentWord.Contains("e")) { 
+                    vowelTotal++;
+                 }
+                if (currentWord.Contains("i"))
+                {
+                    vowelTotal++;
+                }
+                if (currentWord.Contains("o"))
+                {
+                    vowelTotal++;
+                }
+                if (currentWord.Contains("u"))
+                {
+                    vowelTotal++;
+                }
+                //Comparing uppercase
+                if (currentWord.Contains("A"))
+                {
+                    vowelTotal++;
+                }
+                if (currentWord.Contains("E"))
+                {
+                    vowelTotal++;
+                }
+                if (currentWord.Contains("I"))
+                {
+                    vowelTotal++;
+                }
+                if (currentWord.Contains("O"))
+                {
+                    vowelTotal++;
+                }
+                if (currentWord.Contains("U"))
+                {
+                    vowelTotal++;
+                }
+                if(vowelTotal > prevWordTotal)
+                {
+                    mostVowWord = currentWord;
+                }
+                prevWordTotal = vowelTotal;
+
+            }
+
+            MessageBox.Show("The word with the most vowels is: " + mostVowWord);
+        }
+        
+        private void ExitBut_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
